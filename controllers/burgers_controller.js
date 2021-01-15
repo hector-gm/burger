d4095a03a9a45ca221e6d06b6bed98eae46e6012
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-01-15 18:07:24 -0500

    Revised css stylesheet path link

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ const burger = require('../models/burger.js');
 
 // Create all our routes and set up logic within those routes where required.
 router.get('/', (req, res) => {
-    burger.all((data) => {
+    burger.selectAll((data) => {
       const hbsObject = {
         burgers: data,
       };
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
   });
   
   router.post('/api/burgers', (req, res) => {
-    burger.create('burger_name', [req.body.name], (result) => {
+    burger.insertOne('burger_name', [req.body.name], (result) => {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
@@ -28,7 +28,7 @@ router.get('/', (req, res) => {
   
     console.log('condition', condition);
   
-    burger.update(
+    burger.updateOne(
       {
         devoured: req.body.devoured,
       },
--- models/burger.js
+++ models/burger.js
@@ -2,15 +2,18 @@
 const orm = require('../config/orm');
 
 const burger = {
-  all(cb) {
+  selectAll(cb) {
     orm.selectAll('burgers', (res) => cb(res));
   },
   // The variables cols and vals are arrays.
-  create(cols, vals, cb) {
+  insertOne(cols, vals, cb) {
     orm.insertOne('burgers', cols, vals, (res) => cb(res));
   },
-  update(objColVals, condition, cb) {
+  updateOne(objColVals, condition, cb) {
     orm.updateOne('burgers', objColVals, condition, (res) => cb(res));
+  },
+  delete(condition, cb) {
+    orm.delete('burgers', condition, (res) => cb(res));
   }
 };
 
